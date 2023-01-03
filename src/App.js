29569bea04e2f77657ee9226becdef195b06581e
Talita-S <talitassantos2015@gmail.com>
2023-01-03 17:28:54 -0300

    Componentes Navbar e Footer criados e referenciados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
@@ -6,16 +6,13 @@ import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+      <Navbar/>
       <Container customClass="min-height">
         <Routes>
           <Route path= "/" element={<Home/>}/> 
@@ -24,7 +21,7 @@ function App() {
           <Route path= "/newproject" element={<NewProject/>}/>    
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 }
