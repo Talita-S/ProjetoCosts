d676825a575341554ac0e56bd06bfc7b17f0760d
Talita-S <talitassantos2015@gmail.com>
2023-01-05 10:00:56 -0300

    Estruturando e estilizando o botao de criar novo projeto

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import styles from './Home.module.css'
 
 import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
 
 function Home() {
     return(
@@ -9,7 +10,7 @@ function Home() {
                 Bem-vindo ao <span>Costs</span>
             </h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-            <a href="/">Criar projeto</a>
+            <LinkButton to="/newproject" text="Criar Projeto"/>
             <img src={savings} alt="Costs"/>
         </section>
     )
